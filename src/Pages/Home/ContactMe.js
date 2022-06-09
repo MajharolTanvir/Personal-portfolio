d90889992a8dfb5c67c1c9dc11181b3e7a553ea7
Majharul tanvir <mdtanvir6070@gmail.com>
2022-06-09 23:44:34 +0600

    footer

--- src/Pages/Home/ContactMe.js
+++ src/Pages/Home/ContactMe.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FaLinkedin, FaFacebookSquare, FaPhoneAlt, FaWhatsapp } from "react-icons/fa";
+import { AiOutlineMail } from "react-icons/ai";
 
 const ContactMe = () => {
     return (
@@ -8,6 +10,26 @@ const ContactMe = () => {
                 <p></p>
             </div>
             <div className='grid grid-cols-1 md:grid-cols-2'>
+                <div class="flex justify-center items-center">
+                    <div class="block p-6 rounded-lg shadow-md shadow-gray-300 bg-gray-900 max-w-sm">
+                        <div className='flex items-center'>
+                            <AiOutlineMail className='text-4xl mr-3 text-gray-300 my-2' />
+                            <h5 class="text-gray-300 text-xl leading-tight font-medium mb-2"> majharul5456@gmail.com</h5>
+                        </div>
+                        <div className='flex items-center'>
+                            <FaPhoneAlt className='text-4xl mr-3 text-gray-300 my-2' />
+                            <h5 class="text-gray-300 text-xl leading-tight font-medium mb-2"> (+880) 18160-66160 </h5>
+                        </div>
+                        <div className='flex items-center'>
+                            <FaWhatsapp className='text-4xl mr-3 text-gray-300 my-2' />
+                            <h5 class="text-gray-300 text-xl leading-tight font-medium mb-2"> (+880) 18160-66160 </h5>
+                        </div>
+                        <div className='flex justify-around'>
+                            <a target='_blank' rel="noreferrer" href="https://www.facebook.com/MajharulTanvir.rgt/" className='text-4xl '><FaFacebookSquare className='text-gray-300 my-2' /></a>
+                            <a target='_blank' rel="noreferrer" href="https://www.linkedin.com/in/majharul-tanvir-rgt/" className='text-4xl'><FaLinkedin className='text-gray-300 my-2' /></a>
+                        </div>
+                    </div>
+                </div>
                 <div className="block p-6 rounded-lg shadow-lg bg-gray-900 shadow-gray-300 max-w-md">
                     <form>
                         <div className="form-group mb-6">
---
+++ src/Shared/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Footer = () => {
+    let getYear = () => {
+        let currentYear = new Date().getFullYear();
+        return currentYear;
+    };
+    return (
+        <footer className="bg-gray-900 text-center lg:text-left mt-20">
+            <div className="text-gray-300 text-center p-4" style={{ backgroundColor: "rgba(0, 0, 0, 0.2);" }}>
+                <p className="m-0 text-center text-white">
+                    Copyright &copy;
+                    <span> {getYear()} <a target='_blank' rel="noreferrer" className="text-gray-300" href="https://www.facebook.com/MajharulTanvir.rgt/">Made with Tanvir🔥</a> </span>
+                </p>
+            </div>
+        </footer >
+    );
+};
+
+export default Footer;
