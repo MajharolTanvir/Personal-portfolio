f83cb0ad378bef1f5b9248866dcecb42539ee643
Majharul tanvir <mdtanvir6070@gmail.com>
2022-06-10 23:36:06 +0600

    product details added

--- src/Pages/Home/Project.js
+++ src/Pages/Home/Project.js
@@ -32,16 +32,7 @@ const Project = () => {
                             </a>
                             <div className="p-6 text-center">
                                 <h5 className="text-gray-300 text-xl font-medium mb-2">{project.name}</h5>
-                                <div className='flex flex-col'>
-                                    <a target='_blank' rel="noreferrer" href={project.live} >
-                                        <button type="button" className="inline-block px-4 py-1.5 bg-gray-300 text-gray-900 font-bold text-xs leading-tight uppercase rounded hover:text-gray-300 hover: border-gray-300 border-2 shadow-md hover:bg-gray-900 hover:shadow-lg focus:bg-gray-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-800 active:shadow-lg transition duration-150 ease-in-out my-2 w-full">Live site</button>
-                                    </a>
-                                    <a target='_blank' rel="noreferrer" href={project.clientSide} >
-                                        <button type="button" className="inline-block px-4 py-1.5 bg-gray-300 text-gray-900 font-bold text-xs leading-tight uppercase rounded hover:text-gray-300 hover: border-gray-300 border-2 shadow-md hover:bg-gray-900 hover:shadow-lg focus:bg-gray-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-800 active:shadow-lg transition duration-150 ease-in-out my-2 w-full">Client side</button>
-                                    </a>
-                                    {project.serverSide && <a target='_blank' rel="noreferrer" href={project.serverSide} >
-                                        <button type="button" className="inline-block px-4 py-1.5 bg-gray-300 text-gray-900 font-bold text-xs leading-tight uppercase rounded hover:text-gray-300 hover: border-gray-300 border-2 shadow-md hover:bg-gray-900 hover:shadow-lg focus:bg-gray-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-800 active:shadow-lg transition duration-150 ease-in-out my-2 w-full">Server side</button>
-                                    </a>}
+                                <div>
                                     <button onClick={() => handleDetails(project._id)} type="button" className="inline-block px-4 py-1.5 bg-gray-300 text-gray-900 font-bold text-xs leading-tight uppercase rounded hover:text-gray-300 hover: border-gray-300 border-2 shadow-md hover:bg-gray-900 hover:shadow-lg focus:bg-gray-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-800 active:shadow-lg transition duration-150 ease-in-out my-2 w-full">Project details</button>
                                 </div>
                             </div>
--- src/Pages/Home/ProjectDetails.js
+++ src/Pages/Home/ProjectDetails.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
+const port = process.env.PORT || 3000
 
 const ProjectDetails = () => {
-    const id = useParams()
-    const { data: projects, isLoading } = useQuery('projects', () => fetch('data.json').then(res => res.json()))
-    console.log(projects, id)
+    const { id } = useParams()
+    const { data: projects, isLoading } = useQuery('projects', () => fetch(`http://localhost:${port}/data.json`).then(res => res.json()))
 
     if (isLoading) {
         return <div className="spinner-grow inline-block w-12 h-12 bg-current rounded-full opacity-0" role="status">
@@ -13,18 +13,32 @@ const ProjectDetails = () => {
         </div>
     }
 
-    // const project = projects.filter(id === projects._id)
-    // console.log(project);
+    const project = projects.find(project => id === project._id)
 
     return (
-        <div className="flex justify-center">
-            <div className="block p-6 rounded-lg shadow-lg bg-white max-w-sm">
-                <h5 className="text-gray-900 text-xl leading-tight font-medium mb-2">Card title</h5>
-                <p className="text-gray-700 text-base mb-4">
-                    Some quick example text to build on the card title and make up the bulk of the card's
-                    content.
-                </p>
-                <button type="button" className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Button</button>
+        <div className="flex justify-center ">
+            <div className="rounded-lg shadow-lg shadow-gray-300 bg-gray-900 max-w-sm">
+                <a href="#!">
+                    <img className="rounded-t-lg" src={project.image} alt="" />
+                </a>
+                <h4 className='text-gray-300 text-2xl mt-5 font-bold'>{project.name}</h4>
+                <div className="p-6">
+                    <div class="flex justify-center">
+                        <ul class="bg-gray-900  rounded-lg border border-gray-200 w-96 text-gray-300">
+                            <li class="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg">{project.about}</li>
+                            <li class="px-6 py-2 w-full rounded-b-lg">{project.techonology}</li>
+                        </ul>
+                    </div>
+                    <a target='_blank' rel="noreferrer" href={project.live} >
+                        <button type="button" className="inline-block px-4 py-1.5 bg-gray-300 text-gray-900 font-bold text-xs leading-tight uppercase rounded hover:text-gray-300 hover: border-gray-300 border-2 shadow-md hover:bg-gray-900 hover:shadow-lg focus:bg-gray-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-800 active:shadow-lg transition duration-150 ease-in-out my-2 w-full">Live site</button>
+                    </a>
+                    <a target='_blank' rel="noreferrer" href={project.clientSide} >
+                        <button type="button" className="inline-block px-4 py-1.5 bg-gray-300 text-gray-900 font-bold text-xs leading-tight uppercase rounded hover:text-gray-300 hover: border-gray-300 border-2 shadow-md hover:bg-gray-900 hover:shadow-lg focus:bg-gray-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-800 active:shadow-lg transition duration-150 ease-in-out my-2 w-full">Client side</button>
+                    </a>
+                    {project.serverSide && <a target='_blank' rel="noreferrer" href={project.serverSide} >
+                        <button type="button" className="inline-block px-4 py-1.5 bg-gray-300 text-gray-900 font-bold text-xs leading-tight uppercase rounded hover:text-gray-300 hover: border-gray-300 border-2 shadow-md hover:bg-gray-900 hover:shadow-lg focus:bg-gray-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-800 active:shadow-lg transition duration-150 ease-in-out my-2 w-full">Server side</button>
+                    </a>}
+                </div>
             </div>
         </div>
     );
