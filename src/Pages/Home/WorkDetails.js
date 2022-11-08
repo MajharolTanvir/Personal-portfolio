408782f5571fd641f1f6f8008dea6bf61c1ca7c7
unknown <mdtanvir6070@gmail.com>
2022-11-08 12:52:52 +0600

    my services

--- src/Pages/Home/WorkDetails.js
+++ src/Pages/Home/WorkDetails.js
@@ -1,36 +1,54 @@
-import React from 'react';
-import Frontend from '../../Utilities/work/frontend.jpg'
-import Backend from '../../Utilities/work/backend.jpg'
-import MERN from '../../Utilities/work/mern.jpg'
+import React from "react";
+import Frontend from "../../Utilities/work/frontend.jpg";
+import Backend from "../../Utilities/work/backend.jpg";
+import MERN from "../../Utilities/work/mern.jpg";
+import { AiFillCaretRight } from "react-icons/ai";
 
 const WorkDetails = () => {
-    return (
-        <div id='service' className="container mx-auto px-3 md:px-0">
-            <div className='mb-20 text-gray-300'>
-                <h4 className='font-bold text-3xl'>Service</h4>
-                <p>The jobs that I am experienced and work on About it is given below.</p>
-            </div>
-            <div className="rounded-lg bg-gray-900">
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-center gap-10">
-                    <div className="block p-6 text-gray-300 rounded-lg shadow-md bg-gray-900 max-w-sm shadow-gray-300">
-                        <img src={Frontend} alt="" />
-                        <h5 className=" mt-3 text-xl leading-tight font-medium mb-2">Frontend developer</h5>
-                        <p>Html, Css, Bootstrap, Tailwind, Javascript, React.</p>
-                    </div>
-                    <div className="block p-6 text-gray-300 rounded-lg shadow-md bg-gray-900 max-w-sm shadow-gray-300">
-                        <img src={Backend} alt="" />
-                        <h5 className="mt-3 text-xl leading-tight font-medium mb-2">Backend developer</h5>
-                        <p className="text-gray-300 text-base mb-4">Node, Express, MongoDB.</p>
-                    </div>
-                    <div className="block p-6 rounded-lg text-gray-300 shadow-md bg-gray-900 max-w-sm shadow-gray-300">
-                        <img src={MERN} alt="" />
-                        <h5 className="mt-3 text-xl leading-tight font-medium mb-2">MERN stack developer</h5>
-                        <p className="text-gray-300 text-base mb-4">Html, Css, Bootstrap, Tailwind, Javascript, React, Node, Express, MongoDB</p>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div id="service" className="container mx-auto mt-20">
+      <div className="mx-5 md:mx-0">
+        <div className=" mb-10 flex justify-center items-center gap-2 text-cyan-300">
+          <h4 className="text-xl md:text-2xl lg:text-3xl font-bold">
+            My services
+          </h4>
+          <AiFillCaretRight />
         </div>
-    );
+        <div className="rounded-lg bg-gray-900">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-center gap-10">
+            <div className="block p-6 text-cyan-300 rounded-lg shadow-md bg-gray-900 max-w-sm shadow-cyan-300">
+              <img src={Frontend} alt="" />
+              <h5 className=" mt-3 text-xl leading-tight font-medium mb-2">
+                Frontend developer
+              </h5>
+              <p>
+                Html, Css, Bootstrap, Tailwind, Javascript, React, TypeScript,
+                DaisyUI, ANT design, Redux.
+              </p>
+            </div>
+            <div className="block p-6 text-cyan-300 rounded-lg shadow-md bg-gray-900 max-w-sm shadow-cyan-300">
+              <img src={Backend} alt="" />
+              <h5 className="mt-3 text-xl leading-tight font-medium mb-2">
+                Backend developer
+              </h5>
+              <p className="text-cyan-300 text-base mb-4">
+                Node.js, Express.js, MongoDB, FireStore.
+              </p>
+            </div>
+            <div className="block p-6 rounded-lg text-cyan-300 shadow-md bg-gray-900 max-w-sm shadow-cyan-300">
+              <img src={MERN} alt="" />
+              <h5 className="mt-3 text-xl leading-tight font-medium mb-2">
+                WordPress developer
+              </h5>
+              <p className="text-cyan-300 text-base mb-4">
+                WordPress with Elementor , so many Theme, Plugin and Templates.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default WorkDetails;
